64073fe693ed5ddc4f6a9ecbd0842a1a8806577e
deo002012 <deolucky15@gmail.com>
2020-08-04 16:54:53 +0530

    update

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -9,11 +9,22 @@ import { MatDialogRef ,MAT_DIALOG_DATA} from '@angular/material/dialog';
 })
 export class DetailsComponent implements OnInit {
 
-    row;
-    constructor(@Inject(MAT_DIALOG_DATA) public arr: string,
+    row=[];
+    keys=[];
+    // row1:string;
+    // newrow=[];
+    // newrow1=[];
+    constructor(@Inject(MAT_DIALOG_DATA) public arr: {},
     private dialogRef: MatDialogRef<DetailsComponent>) {
-      this.row=arr;
-          console.log(this.row);
+      this.row=Object.values(arr);
+      this.keys=Object.keys(arr);
+      // -----BY COVERTING THE OBJECT INTO STRING BY STRINGIFY-----
+      // this.row=JSON.stringify(arr);
+      // this.row1=this.row.replace('"Options":{',"").replace(`"Tags":[{`,"").replace(`}`,"").replace(`}`,"").replace(`}`,"").replace(`}]}`,"").replace(`[{`,"").replace(`"Tags":[]}`,"").replace(`[`,"").replace(`{`,"").replace(`]`,"").replace(`]`,"").replace(`}]`,"");
+      // this.newrow=this.row1.split(',');
+      // this.newrow1=this.newrow.splice(1,this.newrow.length);
+        // console.log(this.newrow);
+        //   console.log(this.row);
      }
 
    
